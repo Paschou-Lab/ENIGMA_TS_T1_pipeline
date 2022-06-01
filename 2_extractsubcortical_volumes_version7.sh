55c3a778cea82c758d5783eaf32bc801ea5d9c65
yinjin0527 <43553886+yinjin0527@users.noreply.github.com>
2022-06-01 04:59:48 -0400

    Update 2_extractsubcortical_volumes_version7.sh

--- 2_extractsubcortical_volumes_version7.sh
+++ 2_extractsubcortical_volumes_version7.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #####
 # 10/23/15
-# Wrapper written by Eric Kan (ekan@chla.usc.edu)
+# 
 #
 #	
 # Script assumes the following structure
@@ -39,11 +39,11 @@ mkdir ${enigmadir}/outputs/subcortical #JA
 
 cd ${enigmadir}/outputs
 echo ${PWD}
-echo "SubjID,LLatVent,RLatVent,Lthal,Rthal,Lcaud,Rcaud,Lput,Rput,Lpal,Rpal,Lhippo,Rhippo,Lamyg,Ramyg,Laccumb,Raccumb,ICV" > subcortical/LandRvolumes.csv
+echo "SubjID,LLatVent,RLatVent,3Ventricle, 4Ventricle, Lthal,Rthal,Lcaud,Rcaud,Lput,Rput,Lpal,Rpal,Lhippo,Rhippo,Lamyg,Ramyg,Laccumb,Raccumb,BrainStem,ICV" > subcortical/LandRvolumes.csv
 for subj_id in `ls -d *subj*`
 do 
 	printf "%s,"  "${subj_id}" >> subcortical/LandRvolumes.csv  #JA
-	for x in Left-Lateral-Ventricle Right-Lateral-Ventricle Left-Thalamus-Proper Right-Thalamus-Proper Left-Caudate Right-Caudate Left-Putamen Right-Putamen Left-Pallidum Right-Pallidum Left-Hippocampus Right-Hippocampus Left-Amygdala Right-Amygdala Left-Accumbens-area Right-Accumbens-area 
+	for x in Left-Lateral-Ventricle Right-Lateral-Ventricle 3rd-Ventricle 4th-Ventricle Left-Thalamus Right-Thalamus Left-Caudate Right-Caudate Left-Putamen Right-Putamen Left-Pallidum Right-Pallidum Left-Hippocampus Right-Hippocampus Left-Amygdala Right-Amygdala Left-Accumbens-area Right-Accumbens-area Brain-Stem
 	do
 		printf "%g," `grep  ${x} ${subj_id}/stats/aseg.stats | awk '{print $4}'` >> subcortical/LandRvolumes.csv  #JA
 	done
@@ -61,31 +61,7 @@ cp ${enigmadir}/outputs/subcortical/LandRvolumes.csv ${enigmadir}/outputs/subcor
 cd ${enigmadir}/outputs/subcortical/figures  #JA
 R --no-save --slave < ${enigmadir}/enigma_wrapscripts/R/ENIGMA_plots.R
 
-###############
-# Perform outlier detection
-cd ${enigmadir}/outputs/subcortical/figures  #JA
-${enigmadir}/enigma_wrapscripts/bash/mkIQIrange.sh > jnk.txt
-more jnk.txt | grep "has" |  awk -F/ ' { print $NF } ' | awk ' { print $1 } '| sort | uniq > jnk2.txt
-more jnk2.txt | wc -l
-hd=`pwd`
 
-num=`cat jnk2.txt | wc | awk '{print $1}'`
-echo "There are ${num} subjects labeled as outliers"
-echo "Would you like to review them now? (type y or n)"
-read resp
-
-if [ ${resp} == "y" ]; then
-	for img in `more jnk2.txt`
-	do 
-		echo $img 
-		cd ${enigmadir}/outputs/$img/mri/
-		mri_convert --out_orientation RAS --in_type mgz --out_type nii T1.mgz T1.nii ; 
-		mri_convert --out_orientation RAS --in_type mgz --out_type nii aseg.mgz aseg.nii ; 
-		more $hd/jnk.txt | grep $img
-		fslview T1.nii aseg.nii -t 0.2 -l "MGH-Subcortical"; 
-		rm *.nii
-	done
-fi
 
 #################
 # Create PNGs for QC
@@ -100,7 +76,7 @@ for x in `ls -d ${fsdir}/*subj*`
 do
 	export sub=`basename ${x}`
 	echo ${sub}
-	matlab  -nosplash < ${enigmadir}/enigma_wrapscripts/Matlab/create_pngs_subcortical.m  #JA
+	    matlab  -nosplash < ${enigmadir}/enigma_wrapscripts/Matlab/create_pngs_subcortical.m
 done
 
 #################
@@ -109,3 +85,31 @@ cd ${enigmadir}/outputs/subcortical/QC
 
 chmod 777 make_subcortical_ENIGMA_QC_webpage.sh  #JA
 ${enigmadir}/enigma_wrapscripts/bash/make_subcortical_ENIGMA_QC_webpage.sh ${enigmadir}/outputs/subcortical/QC  #JA
+
+
+###############
+# Perform outlier detection
+cd ${enigmadir}/outputs/subcortical/figures  #JA
+${enigmadir}/enigma_wrapscripts/bash/mkIQIrange.sh > jnk.txt
+more jnk.txt | grep "has" |  awk -F/ ' { print $NF } ' | awk ' { print $1 } '| sort | uniq > jnk2.txt
+more jnk2.txt | wc -l
+hd=`pwd`
+
+num=`cat jnk2.txt | wc | awk '{print $1}'`
+echo "There are ${num} subjects labeled as outliers"
+echo "Would you like to review them now? (type y or n)"
+read resp
+
+if [ ${resp} == "y" ]; then
+    for img in `more jnk2.txt`
+    do
+        echo $img
+        cd ${enigmadir}/outputs/$img/mri/
+        mri_convert --out_orientation RAS --in_type mgz --out_type nii T1.mgz T1.nii ;
+        mri_convert --out_orientation RAS --in_type mgz --out_type nii aseg.mgz aseg.nii ;
+        more $hd/jnk.txt | grep $img
+        fslview T1.nii aseg.nii -t 0.2 -l "MGH-Subcortical";
+        rm *.nii
+    done
+fi
+
